cc344d0f27907512ed273949bc3afa1e11b7f5de
mblanke70 <blanke@ursulaschule.de>
2018-12-13 21:40:26 +0100

    ...

--- resources/views/wahlfach.blade.php
+++ resources/views/wahlfach.blade.php
@@ -10,73 +10,77 @@
 
     <h2>Gewählte Fächer bzw. Kurse</h2>
 
-    <table class="table table-bordered table-striped table-dark table-sm">
-       <tr>
-            <th scope="col">Typ</th>
-            <th scope="col">Fach</th>
-            <th scope="col">Lernfeld</th>
-            <th scope="col">Stunden</th>
-            <th scope="col">Halbjahre</th>
-            <th scope="col">Einbringung</th>
-        </tr>
+    <div class="table-responsive">
 
-    @foreach($matrix as $fachwahl)
-        <tr>
-            <td scope="col">
-                @if( $fachwahl['typ'] == 'wf')
-                    <a href="#" onclick="event.preventDefault(); document.getElementById('delete-form-{{ $fachwahl['fach']->code }}').submit();">
-                        <i class="fa fa-fw fa-trash" style="color:white"></i>
-                    </a>
-                   
-                    <form id="delete-form-{{ $fachwahl['fach']->code }}" action="{{ url($stufe->code) }}" method="POST" style="display: none;">
+        <table class="table table-bordered table-striped table-dark table-sm">
+           <tr>
+                <th scope="col">Typ</th>
+                <th scope="col">Fach</th>
+                <th scope="col">Lernfeld</th>
+                <th scope="col">Stunden</th>
+                <th scope="col">Halbjahre</th>
+                <th scope="col">Einbringung</th>
+            </tr>
+
+        @foreach($matrix as $fachwahl)
+            <tr>
+                <td scope="col">
+                    @if( $fachwahl['typ'] == 'wf')
+                        <a href="#" onclick="event.preventDefault(); document.getElementById('delete-form-{{ $fachwahl['fach']->code }}').submit();">
+                            <i class="fa fa-fw fa-trash" style="color:white"></i>
+                        </a>
+                       
+                        <form id="delete-form-{{ $fachwahl['fach']->code }}" action="{{ url($stufe->code) }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <input type="hidden" name="wahlfach" value="{{ $fachwahl['fach']->code }}" />
+                        </form>
+                    @else
+                        {{ $fachwahl['typ'] }}
+                    @endif
+                </td>
+                <td>{{ $fachwahl['fach']->name }}</td>
+                <td>
+                    @if( isset($fachwahl['fach']->lf) )
+                        {{ chr(65 + $fachwahl['fach']->lf) }}
+                    @endif
+                </td>
+                <td>{{ $fachwahl['stunden'] }}</td>
+                <td>
+                    @if($fachwahl['halbjahre'] == 2)
+                    <form action="{{ url($stufe->code.'/'.$fachwahl['fach']->code) }}" method="POST">
                         {{ csrf_field() }}
-                        {{ method_field('DELETE') }}
-                        <input type="hidden" name="wahlfach" value="{{ $fachwahl['fach']->code }}" />
+
+                        <div class="form-check form-check-inline">
+                            <input type="radio" id="halbjahre[{{$fachwahl['typ']}}]" name="halbjahre" value="2" class="form-check-input" @if( $fachwahl['halbjahre'] == 2) checked="checked" @endif>
+                            <label class="form-check-label" for="halbjahre[{{$fachwahl['typ']}}]">2</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input type="radio" id="halbjahre[{{$fachwahl['typ']}}]" name="halbjahre" value="4" class="form-check-input" @if( $fachwahl['halbjahre'] == 4) checked="checked" @endif>
+                            <label class="form-check-label" for="halbjahre[{{$fachwahl['typ']}}]">4</label>
+                        </div>
                     </form>
-                @else
-                    {{ $fachwahl['typ'] }}
-                @endif
-            </td>
-            <td>{{ $fachwahl['fach']->name }}</td>
-            <td>
-                @if( isset($fachwahl['fach']->lf) )
-                    {{ chr(65 + $fachwahl['fach']->lf) }}
-                @endif
-            </td>
-            <td>{{ $fachwahl['stunden'] }}</td>
-            <td>
-                @if($fachwahl['halbjahre'] == 2)
-                <form action="{{ url($stufe->code.'/'.$fachwahl['fach']->code) }}" method="POST">
-                    {{ csrf_field() }}
+                    @else
+                        {{ $fachwahl['halbjahre'] }}
+                    @endif     
+                </td>
+                <td>{{ $fachwahl['einbringung'] }} Kurse</td>
+            </tr>
+        @endforeach
 
-                    <div class="form-check form-check-inline">
-                        <input type="radio" id="halbjahre[{{$fachwahl['typ']}}]" name="halbjahre" value="2" class="form-check-input" @if( $fachwahl['halbjahre'] == 2) checked="checked" @endif>
-                        <label class="form-check-label" for="halbjahre[{{$fachwahl['typ']}}]">2</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        <input type="radio" id="halbjahre[{{$fachwahl['typ']}}]" name="halbjahre" value="4" class="form-check-input" @if( $fachwahl['halbjahre'] == 4) checked="checked" @endif>
-                        <label class="form-check-label" for="halbjahre[{{$fachwahl['typ']}}]">4</label>
-                    </div>
-                </form>
-                @else
-                    {{ $fachwahl['halbjahre'] }}
-                @endif     
-            </td>
-            <td>{{ $fachwahl['einbringung'] }} Kurse</td>
-        </tr>
-    @endforeach
+            <tr>
+                <td scope="col"></td>
+                <td></td>
+                <td style="text-align: right;">Summe:</td>
+                <td colspan="2">
+                    @if(isset($summe1)) {{ $summe1 }} Kurse @endif
+                    @if(isset($summe2)) ({{ $summe2 }} Stunden) @endif
+                </td>
+            </tr>
 
-        <tr>
-            <td scope="col"></td>
-            <td></td>
-            <td style="text-align: right;">Summe:</td>
-            <td colspan="2">
-                @if(isset($summe1)) {{ $summe1 }} Kurse @endif
-                @if(isset($summe2)) ({{ $summe2 }} Stunden) @endif
-            </td>
-        </tr>
-
-    </table>
+        </table>
+        
+    </div>
 
 @endsection
 
