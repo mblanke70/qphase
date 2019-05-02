9253ddce09e85cc7c371ae4ddbada86ff1bc76d3
mblanke70 <blanke@ursulaschule.de>
2019-05-02 16:44:26 +0200

    ...

--- app/Http/Controllers/SportwahlenController.php
+++ app/Http/Controllers/SportwahlenController.php
@@ -14,7 +14,7 @@ class SportwahlenController extends Controller
 {
 	public function __construct()
     {
-        $this->middleware('auth.basic');
+        $this->middleware('user');
     }
 
     public function index()
