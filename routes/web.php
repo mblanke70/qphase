d3ab6b3d295239a76b65e7d1162ca5029b3c1389
mblanke70 <blanke@ursulaschule.de>
2019-05-14 00:12:30 +0200

    ...

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('test', function () {
 Route::group([
         'prefix'     => 'sport', 
         'namespace'  => 'sport',
-        'middleware' => ['web']
+        'middleware' => ['user']
     ], function () 
 {
 	Route::get ('/', 'SportwahlenController@index');
