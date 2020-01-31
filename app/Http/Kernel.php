5ab106af1b0b13f52e9003d5724851279a795fee
mblanke70 <blanke@ursulaschule.de>
2020-01-31 23:58:49 +0100

    ...

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends HttpKernel
     protected $middlewareGroups = [
         'web' => [
             \Illuminate\Session\Middleware\StartSession::class,
-            //\App\Http\Middleware\EncryptCookies::class,
+            \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
